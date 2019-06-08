fca66180e9721aa064ea29c45b809c43bbe72e7b
beatrice-mess <beatricemessano99@gmail.com>
2019-06-08 16:42:23 +0200

    aggiornamento file vm e print

--- src/print.c
+++ src/print.c
@@ -1,7 +1,21 @@
 #include "interfaces.h"
 #include "read.h"
 #include "print.h"
+#include <stdio.h>
+
 
 void stampa(char *str, table *t) {
-    
+    int i = 0;
+    int len = 0;
+    int *arr = load(&(*str), &len);
+
+
+
+    if(arr != NULL) {
+        for(i=0; i<len; i++) {
+
+        }
+
+    }
+
 }
--- src/vm.c
+++ src/vm.c
@@ -1,9 +1,9 @@
-/*Nicolò Bertocco 873896 - Beatrice Messano NUMEROMATRICOLA*/
+/*Nicolò Bertocco 873896 - Beatrice Messano 876673*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include <ctype.h>
+#include <ctype.h> /*dichiara diverse funzioni utili per testare e mappare caratteri.*/
 #include "interfaces.h"
 #include "execute.h"
 
