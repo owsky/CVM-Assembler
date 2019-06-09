5c58db03aaa269bbef15ee081d1e8642bac1f2ea
beatrice-mess <50874304+beatrice-mess@users.noreply.github.com>
2019-06-09 16:57:42 +0200

    Update print.c

--- src/print.c
+++ src/print.c
@@ -1,65 +1,57 @@
-/*Nicolò Bertocco 873896 - Beatrice Messano 876673*/
 #include "interfaces.h"
 #include "read.h"
 #include "print.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-/*
-        [posizione] istruzione P1 P2
-        dove posizione è la posizione nell'array, istruzione il codice mnemonico, P1 e P2 sono gli eventuali parametri.
-        Nel caso uno del parametri sia un registro stampare la rappresentazione mnemonica corrispondente (es: R3 anziché 3)
-        [0] MOV R0 5
-        [3] CALL 8
-        [5] DISPLAY R30
-*/
-table match(table t, int b){
-  while(t){
-    if(b == t-> cod){
-      return t;
+
+table mat(table t, int b){
+    while(t){
+        if(b == t-> cod){
+            return t;
+        }
+        t= t->next;
     }
-   t= t->next;
-  }
-  return NULL;
+    return NULL;
 }
 
-void stampa(char *str, table t) {
+void stam(char *str, table t) {
     int i = 0,  len = 0, *arr = load(&(*str), &len);
     table c;
 
     if(arr != NULL){
-      while(i<len){
-        c = match(t, arr[i]);
-        printf("[%d], %s", i , c-> name);
-        i++;
-        if(t->par>0){
+        while(i< len){
+            if((c = mat(t, arr[i])) != NULL){
 
-            if(!strcmp(t-> p1, "reg")){
-              printf("R%d", arr[i]);
+                printf("[%d] %s ", i , c-> name);
+                i++;
+                if(c->par >0  ){
+                    i++;
+                    if(!strcmp(c-> p1, "reg")){
+                        printf("R%d", arr[i]);
+                    }
+                    else {
+                        printf("%d", arr[i]);
+                    }
+                    if(c-> par == 2) {
+                        i++;
+                        if(!strcmp(c-> p2, "reg")){
+                            printf(" R%d", arr[i]);
+                        }
+                        else {
+                            printf(" %d", arr[i]);
+                        }
+                    }
+                    printf("\n" );
+                }
 
+                else {
+                    printf("\n");
+                }
             }
-            else {
-              printf("%d", arr[i]);
-            }
-            if(t-> p2 != NULL){
-              if(!strcmp(t-> p2, "reg")){
-                printf("R%d", arr[i+1]);
-
-              }
-              else {
-                printf("%d", arr[i+1]);
-
-              }
-            }
-
-
-
-      } else
-        printf("\n");
-
-
-
-
-      }
+            i++;
+        }
     }
-  }
+    free(arr);
+}
