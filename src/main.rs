eaa4afea7f871dcb2d47ab81b9f68562f7e53057
Nicolò Bertocco <nick.bertocco@gmail.com>
2022-04-03 11:38:46 +0200

    Commented code

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,14 @@ use std::env;
 use std::io::BufRead;
 
 fn main() {
+    // checks if the user provided the correct amount of cli arguments
     let args: Vec<String> = env::args().collect();
     if args.len() == 2 {
         let file = read_input::read_program_from_file(&args[1]);
         let re = Regex::new(";.*").unwrap();
         let mut program: Vec<i32> = Vec::new();
+
+        // reads provided program line by line and strips it of unneeded lines and characters
         for (_index, line) in file.lines().enumerate() {
             let mut line = line.unwrap();
             let filtered_line = re.replace_all(&mut line, "");
@@ -20,7 +23,10 @@ fn main() {
                 program.push(trimmed_filtered_line.parse::<i32>().unwrap());
             }
         }
+
+        // removes program length line
         let program_sliced = &program[1..];
+
         let mut assembler = Assembler::new();
         assembler.execute(program_sliced);
     }
--- src/vm.rs
+++ src/vm.rs
@@ -3,8 +3,10 @@ use std::{collections::HashMap, usize};
 const STACK_SIZE: i32 = 120;
 pub struct Assembler {
     registers: [i32; 32],
-    ip: i32,
+    instruction_pointer: i32,
     stack: Vec<i32>,
+    // the function definition uses a vector to pass arguments to achieve compatibility with all the functions
+    // if a function tries to access more arguments than the ones defined in the spec sheet it will panic
     functions_table: HashMap<i32, fn(&mut Self, args: &Vec<i32>)>,
 }
 
@@ -27,20 +29,22 @@ impl Assembler {
         self.functions_table.insert(33, Self::div);
     }
 
+    // constructor
     pub fn new() -> Assembler {
         let mut new_assembler = Assembler {
             registers: [0; 32],
             functions_table: HashMap::new(),
-            ip: 0,
+            instruction_pointer: 0,
             stack: Vec::new(),
         };
         new_assembler.init_map();
         return new_assembler;
     }
 
+    // interprets the program line by line and executes it
     pub fn execute(&mut self, program: &[i32]) {
         loop {
-            let func_code = program[(self.ip) as usize];
+            let func_code = program[(self.instruction_pointer) as usize];
             if func_code == 0 {
                 return;
             }
@@ -58,19 +62,25 @@ impl Assembler {
                 .expect("function not found");
             let mut args: Vec<i32> = Vec::new();
             for i in 0..arg_size {
-                let arg_code = program[(self.ip + i + 1) as usize];
+                let arg_code = program[(self.instruction_pointer + i + 1) as usize];
                 args.push(arg_code);
             }
             func(self, &args);
         }
     }
 
+    // Prints the value stored in the given register
+    // Function code: 1
+    // Parameters: Registry<R0-R31>
     fn display(&mut self, args: &Vec<i32>) {
         let reg = args[0];
         println!("{}", self.registers[reg as usize]);
-        self.ip += 2;
+        self.instruction_pointer += 2;
     }
 
+    // Prints n numbers stored on the stack
+    // Function code: 2
+    // Parameters: Integer
     fn print_stack(&mut self, args: &Vec<i32>) {
         let mut num = args[0];
         while num > 0 {
@@ -78,18 +88,22 @@ impl Assembler {
             println!("{} {}", num - 1, el);
             num -= 1;
         }
-        self.ip += 2;
+        self.instruction_pointer += 2;
     }
 
+    // Inserts the value stored in the given register into the stack
+    // Function code: 10
+    // Parameters: Registry<R0-R31>
     fn push(&mut self, args: &Vec<i32>) {
         if self.stack.len() as i32 >= STACK_SIZE {
             Assembler::stack_overflow();
         }
         let reg = args[0];
         self.stack.push(self.registers[reg as usize]);
-        self.ip += 2;
+        self.instruction_pointer += 2;
     }
 
+    // Inserts the number into the stack, for internal implementations
     fn push_internal(&mut self, num: i32) {
         if self.stack.len() as i32 >= STACK_SIZE {
             Assembler::stack_overflow();
@@ -97,68 +111,96 @@ impl Assembler {
         self.stack.push(num);
     }
 
+    // Removes one number from the stack and stores it into the given register
+    // Function code: 11
+    // Parameters: Registry<R0-R31>
     fn pop(&mut self, args: &Vec<i32>) {
         let reg = args[0];
         self.registers[reg as usize] = self.stack.pop().expect("Stack underflow");
-        self.ip += 2;
+        self.instruction_pointer += 2;
     }
 
+    // Pops one item from the stack, for internal implementations
     fn pop_internal(&mut self) -> i32 {
         return self.stack.pop().expect("Stack underflow");
     }
 
+    // Stores the integer into the given register
+    // Function code: 12
+    // Parameters: Registry<R0-R31> Integer
     fn mov(&mut self, args: &Vec<i32>) {
         let reg = args[0];
         let num = args[1];
         self.registers[reg as usize] = num;
-        self.ip += 3;
+        self.instruction_pointer += 3;
     }
 
+    // Pushes the next instruction pointer and jumps to the given position
+    // Function code: 20
+    // Parameters: Integer
     fn call(&mut self, args: &Vec<i32>) {
-        self.push_internal(self.ip + 2);
+        self.push_internal(self.instruction_pointer + 2);
         let pos = args[0];
-        self.ip = pos;
+        self.instruction_pointer = pos;
     }
 
+    // Pops the instruction pointer from the stack and jumps back to it
+    // Function code: 21
+    // Parameters: none
     fn ret(&mut self, _args: &Vec<i32>) {
-        self.ip = self.pop_internal();
+        self.instruction_pointer = self.pop_internal();
     }
 
+    //Jumps to the given position
+    // Function code: 22
+    // Parameters: Integer
     fn jmp(&mut self, args: &Vec<i32>) {
         let pos = args[0];
-        self.ip = pos;
+        self.instruction_pointer = pos;
     }
 
+    // Pops one number from the stack, and jumps to it if it's different than zero
+    // Function code: 23
+    // Parameters: Integer
     fn jz(&mut self, args: &Vec<i32>) {
         let pos = args[0];
         let num = self.pop_internal();
         if num == 0 {
-            self.ip = pos;
+            self.instruction_pointer = pos;
         } else {
-            self.ip += 2;
+            self.instruction_pointer += 2;
         }
     }
 
+    // Pops one number from the stack, and jumps to it if it's greater than zero
+    // Function code: 24
+    // Parameters: Integer
     fn jpos(&mut self, args: &Vec<i32>) {
         let pos = args[0];
         let num = self.pop_internal();
         if num > 0 {
-            self.ip = pos;
+            self.instruction_pointer = pos;
         } else {
-            self.ip += 2;
+            self.instruction_pointer += 2;
         }
     }
 
+    // Pops one number from the stack, and jumps to it if it's smaller than zero
+    // Function code: 25
+    // Parameters: Integer
     fn jneg(&mut self, args: &Vec<i32>) {
         let pos = args[0];
         let num = self.pop_internal();
         if num < 0 {
-            self.ip = pos;
+            self.instruction_pointer = pos;
         } else {
-            self.ip += 2;
+            self.instruction_pointer += 2;
         }
     }
 
+    // Adds the values stored in the given registers and pushes the result into the stack
+    // Function code: 30
+    // Parameters: Registry<R0-R1> Registry<R0-R31>
     fn add(&mut self, args: &Vec<i32>) {
         let reg1 = args[0] as i64;
         let reg2 = args[1] as i64;
@@ -169,9 +211,12 @@ impl Assembler {
             Assembler::underflow();
         }
         self.push_internal(self.registers[reg1 as usize] + self.registers[reg2 as usize]);
-        self.ip += 3;
+        self.instruction_pointer += 3;
     }
 
+    // Subtracts the values stored in the given registers and pushes the result into the stack
+    // Function code: 31
+    // Parameters: Registry<R0-R1> Registry<R0-R31>
     fn sub(&mut self, args: &Vec<i32>) {
         let reg1 = args[0] as i64;
         let reg2 = args[1] as i64;
@@ -182,9 +227,12 @@ impl Assembler {
             Assembler::underflow();
         }
         self.push_internal(self.registers[reg1 as usize] - self.registers[reg2 as usize]);
-        self.ip += 3;
+        self.instruction_pointer += 3;
     }
 
+    // Multiplies the values stored in the given registers and pushes the result into the stack
+    // Function code: 32
+    // Parameters: Registry<R0-R1> Registry<R0-R31>
     fn mul(&mut self, args: &Vec<i32>) {
         let reg1 = args[0] as i64;
         let reg2 = args[1] as i64;
@@ -195,9 +243,12 @@ impl Assembler {
             Assembler::underflow();
         }
         self.push_internal(self.registers[reg1 as usize] * self.registers[reg2 as usize]);
-        self.ip += 3;
+        self.instruction_pointer += 3;
     }
 
+    // Divides the values stored in the given registers and pushes the result into the stack
+    // Function code: 33
+    // Parameters: Registry<R0-R1> Registry<R0-R31>
     fn div(&mut self, args: &Vec<i32>) {
         let reg1 = args[0] as usize;
         let reg2 = args[1] as usize;
@@ -205,7 +256,7 @@ impl Assembler {
             panic!("Error: can't divide by zero");
         } else {
             self.push_internal(self.registers[reg1] / self.registers[reg2]);
-            self.ip += 3;
+            self.instruction_pointer += 3;
         }
     }
 
