3ee632a3bcd339759b71b3e65b3105366e185eaa
Nicolò Bertocco <nick.bertocco@gmail.com>
2025-04-30 08:34:10 +0200

    removed unneeded files

--- target/rls/debug/deps/cvm-4fc1bead998d6759.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/owsky/Desktop/CVM-Assembler/target/rls/debug/deps/cvm-4fc1bead998d6759.rmeta: src/main.rs
-
-/mnt/c/Users/owsky/Desktop/CVM-Assembler/target/rls/debug/deps/cvm-4fc1bead998d6759.d: src/main.rs
-
-src/main.rs:
--- target/rls/debug/deps/cvm-eb9c04d791fe197b.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/owsky/Desktop/CVM-Assembler/target/rls/debug/deps/cvm-eb9c04d791fe197b.rmeta: src/main.rs
-
-/mnt/c/Users/owsky/Desktop/CVM-Assembler/target/rls/debug/deps/cvm-eb9c04d791fe197b.d: src/main.rs
-
-src/main.rs:
